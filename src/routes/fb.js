10fd1943b6acdfeed5c2a278afc02181ac7c9140
SteveJeong <jst0930@gmail.com>
2022-07-04 19:53:20 +0300

    setup firebase fb.js __layout.svelte

---
+++ src/routes/fb.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZuGFsZIl8Tjz32Z3fscVNUDdet3ruFUI",
+  authDomain: "file-portal-yt-b8012.firebaseapp.com",
+  projectId: "file-portal-yt-b8012",
+  storageBucket: "file-portal-yt-b8012.appspot.com",
+  messagingSenderId: "236542515567",
+  appId: "1:236542515567:web:27b8024b68d06eb3c0309a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
