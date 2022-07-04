89288c362159d3c7a45f8dcab62d4bc6afbeaf85
SteveJeong <jst0930@gmail.com>
2022-07-04 20:56:16 +0300

    login path in __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,1 +1,19 @@
+<script>
+  import App from './fb'
+  import {onMount} from 'svelte'
+  import {getAuth, onAuthStateChanged} from 'firebase/auth'
+  import {goto} from '$app/navigation'
+
+  onMount(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if(user) {
+        console.log('Welcome to the file portal')
+      } else {
+        goto('/login')
+      }
+    })
+  })
+</script>
+
 <slot />
